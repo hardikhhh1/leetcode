4557b9c74226d78f42d761f115b8112fbc3f1da2
Hardy Arora <harora@akamai.com>
2015-07-14 17:54:58 -0400

    added cpp and java solution for question 2

--- src/BinaryTree/Question2/Solution.java
+++ src/BinaryTree/Question2/Solution.java
@@ -9,6 +9,24 @@
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Edge case: If the root is null , return null
+        if(root == null) return null;
+
+        // If the root is itself the node, then it itself is the lca.
+        if(root == p || root == q) return root;
+
+        // We find the lca for the left and right node of the root.
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // If both are not null that means that one node is on the left branch
+        // and the other is on the right branch. So the root itself is the
+        // LCA.
+        if(left != null && right != null) return root;
+
+        // If any one is null return the other, which means we move to the branch
+        // where one of the node is atleast present.
+        return (left != null ? left : right);
 
     }
 }
